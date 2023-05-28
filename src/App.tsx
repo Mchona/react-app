776a16d2d6fe65d747fa03ecd64875af776fdcb7
Mchona <masaru.chona@gmail.com>
2023-05-28 11:45:45 +0900

    環境構築完了

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Chakraui from './Chakraui';
+
+function App() {
+  return (
+    <div>
+      <Chakraui/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Chakraui.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chakraui = () => {
+  return (
+    <div>chakraui</div>
+  )
+};
+
+export default Chakraui
